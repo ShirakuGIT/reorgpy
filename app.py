acbe2f8e08a2dabd82b5bb8ef6ec2ea4dc0e4283
Shivaram Kumar <47910889+ShirakuGIT@users.noreply.github.com>
2022-02-08 23:05:59 +0530

    v.1.1
    Finished adding customizable directories
    - Users can now specify their own directories
    - They can choose to rewrite or create a directory
    - The directories are written to a text file (text file created if not exists)
    - Optimizations done to save memory (technical way of saying I implemented f.close())
    - Formatted the program a little bit
    - Included more comments for better readability

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# Python program that reorgnizes directories
 # Works muliplatform on Windows/Mac/Linux
 # Currently tested on Ubuntu 21.10
 
@@ -7,22 +6,82 @@ import os
 import shutil
 
 # List of file extensions to check
-documents = [".pdf", ".txt", ".odt", ".xlsx", ".xcf", ".py", ".cs"]
+documents = [".pdf", ".txt", ".odt", ".xlsx", ".xcf", ".py", ".cs", ".sh", ".kdenlive", ".xml", ".odp"]
 music = [".mp3", ".wav", ".aac", ".ogg", ".alac"]
 video = [".mp4", ".mov", ".avi", ".webm", ".wmv"]
 downloads = [".deb", ".dmg", ".exe", ".app", ".zip", ".bz2", ".torrent"]
 pics = [".jpg", ".jpeg", ".png", ".gif", ".tiff", ".raw", ".svg", ".bmp"]
 
+# Introductory code to ask for rewriting text file
+print("""
+					PYTHON FILE ORGANIZER v1.1
+			-----------------------------------------------------------
+	""")
 
+
+# Open a text file if not exists to save user directories
+try:
+	user_dir = open('user_directories.txt', "r")
+except:
+	user_dir = open('user_directories.txt', "w+")
+
+# User directory strings
+read = user_dir.read()
+# For reading no. of directories
+length = read.split("\n")
+# The following three lines is just for formatting
+temp = ""
+for x in length:
+	temp += "\t\t\t" + x + "\n"
+
+print(f"""
+			List of user directories: 
+{temp}
+			{str(len(length))} directories present
+	""")
+
+print("""
+			If you want to rewrite/create your directories,
+			type "yes",
+
+			however, if you don't want to do that,
+			type "no"
+	""")
+
+# Checks if user wants to rewrite his directories
+rewrite = input("\t\t\tType here: ")
 
 # List of user directories for the check
-user_doc = r'/home/shiraku/Documents/'
-user_mus = r'/home/shiraku/Music/'
-user_dow = r'/home/shiraku/Downloads/'
-user_pic = r'/home/shiraku/Pictures/'
-user_vid = r'/home/shiraku/Videos/'
-user_hom = r'/home/shiraku/'
-user_mis = r'/home/shiraku/Misc/'
+if read == "" or rewrite == "yes":
+	user_dir = open('user_directories.txt', "w+")
+	print("")
+
+	user_doc = input("Enter path to documents folder       ")
+	user_mus = input("Enter path to music folder           ")
+	user_dow = input("Enter path to downloads folder       ")
+	user_pic = input("Enter path to pictures folder        ")
+	user_vid = input("Enter path to videos folder          ")
+	user_hom = input("Enter path to home directory         ")
+
+	dir_list = [user_doc, user_mus, user_dow, user_pic, user_vid, user_hom]
+	# Writes the directories to text file
+	for i in dir_list:
+		user_dir.write(i + "\n")
+
+	user_dir.close()
+
+elif rewrite == "no":
+	read = read.split("\n")
+	user_doc = read[0]
+	user_mus = read[1]
+	user_dow = read[2]
+	user_pic = read[3]
+	user_vid = read[4]
+	user_hom = read[5]
+
+else:
+	print("Error, try running the program again")
+
 
 # List of all files' filepaths
 all_dir = [user_hom + x for x in os.listdir(user_hom)] + [user_mus + x for x in os.listdir(user_mus)] + [user_pic + x for x in os.listdir(user_pic)] + [user_vid + x for x in os.listdir(user_vid)] + [user_dow + x for x in os.listdir(user_dow)] + [user_doc + x for x in os.listdir(user_doc)]
@@ -31,8 +90,11 @@ all_dir = [user_hom + x for x in os.listdir(user_hom)] + [user_mus + x for x in 
 # Number of files in list
 filelen = len(all_dir)
 
-# For loop iteration to move the files to their directories
+# Closing the file
+user_dir.close()
+
 def mover():
+
 	for i in all_dir:
 
 		
@@ -61,21 +123,21 @@ def mover():
 
 	#Success statement
 	print(fr"""
-	___________________________________________
-			
-			   _
-			  |)`
-			  | |
-			  | |_____
-			 /    (]__)
-			/    (]___)
-		       /    (]___)
-			  ___(]_)
-			 /
-			/
+			-----------------------------------------------------------
+					
+					   _
+					  |)`
+					  | |
+					  | |_____
+					 /    (]__)
+					/    (]___)
+				       /    (]___)
+					  ___(]_)
+					 /
+					/
 
-	   Successfully moved {filelen} files  
-	___________________________________________
+			   Successfully moved {filelen} files  
+			-----------------------------------------------------------
 	 
 	""")		
 
@@ -84,25 +146,24 @@ def mover():
 # The highlight of the code is the mover() function, so see that #
 ##################################################################
 
-# Take user request
-print("\n  Hey! Do you wanna organize your files?\n")
-print("""
-  Type the following numbers to execute the program:
-	
-	0 - Yes, you want to run the program
-	1 - No, Maybe next time
-""")
-user_in = int(input("""
-	Type your response here:
-	
-		"""))
+
 # Driver code
+
+print("""
+			-----------------------------------------------------------
+			Type the following numbers to execute the program:
+		
+			0 - Yes, you want to run the program
+			1 - No, Maybe next time
+	""")
+user_in = int(input("\t\t\tEnter input here: "))	
+
 if user_in == 0:
 	print("\n")
 	mover()
 elif user_in == 1:
 	print("""
-	Okay, see you next time!
+			Okay, see you next time!
 	""")
 
 
